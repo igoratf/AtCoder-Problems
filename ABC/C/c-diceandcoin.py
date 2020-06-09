e16f31a5c4dcd72932367f30ae8d7b782bc8c9bc
igor <igor.atf@gmail.com>
2020-06-08 21:37:14 -0300

    add C - Dice and coin

---
+++ ABC/C/c-diceandcoin.py
@@ -0,0 +1,19 @@
+n,k = map(int, input().split())
+
+probability = 0
+for i in range(1,n+1):
+    p = 1/n
+    p_head = 1/2
+    flips = 0
+    prob = p * p_head**flips
+    score = i
+    
+    while score < k:
+        flips += 1
+        prob = p * p_head**flips
+        score = score * 2
+
+    probability += prob
+
+
+print(probability)
